3057f3ac23751fbd3dc89040b5b80653667b7995
Leerkracht <Leerkracht@TAL-JR-11G8>
2026-04-28 19:28:51 +0200

    Add project files.

---
+++ Eindwerken6Appda2025/frmHoofdformulier.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Eindwerken6Appda2025
+{
+    partial class frmHoofdformulier
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			menuStrip1 = new MenuStrip();
+			wiskundeInoefenenToolStripMenuItem = new ToolStripMenuItem();
+			jaimyDiscriminantToolStripMenuItem = new ToolStripMenuItem();
+			menuStrip1.SuspendLayout();
+			SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			menuStrip1.ImageScalingSize = new Size(24, 24);
+			menuStrip1.Items.AddRange(new ToolStripItem[] { wiskundeInoefenenToolStripMenuItem });
+			menuStrip1.Location = new Point(0, 0);
+			menuStrip1.Name = "menuStrip1";
+			menuStrip1.Size = new Size(800, 33);
+			menuStrip1.TabIndex = 0;
+			menuStrip1.Text = "menuStrip1";
+			// 
+			// wiskundeInoefenenToolStripMenuItem
+			// 
+			wiskundeInoefenenToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { jaimyDiscriminantToolStripMenuItem });
+			wiskundeInoefenenToolStripMenuItem.Name = "wiskundeInoefenenToolStripMenuItem";
+			wiskundeInoefenenToolStripMenuItem.Size = new Size(189, 29);
+			wiskundeInoefenenToolStripMenuItem.Text = "Wiskunde inoefenen";
+			// 
+			// jaimyDiscriminantToolStripMenuItem
+			// 
+			jaimyDiscriminantToolStripMenuItem.Name = "jaimyDiscriminantToolStripMenuItem";
+			jaimyDiscriminantToolStripMenuItem.Size = new Size(273, 34);
+			jaimyDiscriminantToolStripMenuItem.Text = "Jaimy - Discriminant";
+			jaimyDiscriminantToolStripMenuItem.Click += jaimyDiscriminantToolStripMenuItem_Click;
+			// 
+			// frmHoofdformulier
+			// 
+			AutoScaleDimensions = new SizeF(10F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(menuStrip1);
+			MainMenuStrip = menuStrip1;
+			Name = "frmHoofdformulier";
+			Text = "Wiskunde inoefenen";
+			Load += frmHoofdformulier_Load;
+			menuStrip1.ResumeLayout(false);
+			menuStrip1.PerformLayout();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem wiskundeInoefenenToolStripMenuItem;
+		private ToolStripMenuItem jaimyDiscriminantToolStripMenuItem;
+	}
+}
---
+++ Eindwerken6Appda2025/frmHoofdformulier.cs
@@ -0,0 +1,20 @@
+namespace Eindwerken6Appda2025
+{
+	public partial class frmHoofdformulier : Form
+	{
+		public frmHoofdformulier()
+		{
+			InitializeComponent();
+		}
+
+		private void frmHoofdformulier_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void jaimyDiscriminantToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// Kijk in de presentatie hoe je een formulier instantiëert
+		}
+	}
+}
